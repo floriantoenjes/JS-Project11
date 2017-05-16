00bf9a68061d9e6f86111ed9d5ad059af9efdb33
Florian Tönjes <skatepainter@yahoo.de>
2017-05-16 20:46:26 +0200

    Review model running now

--- src/models/review.js
+++ src/models/review.js
@@ -3,13 +3,17 @@
 const mongoose = require("mongoose");
 
 const ReviewSchema = new mongoose.Schema({
-    user: Number,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
     postedOn: {
         type: Date,
         default: Date.now()
     },
     rating: {
         type: Number,
+        required: true,
         min: 1,
         max: 5
     },
