7837ebe8de8ad641317e0e2f7ad1d8079f8c23f2
Florian Tönjes <skatepainter@yahoo.de>
2017-05-17 14:43:48 +0200

    Further implemented get users route

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,18 @@
 "use strict";
 
 const express = require("express");
+const basicAuth = require("basic-auth");
 const router = express.Router();
 const User = require("../models/user");
 
 router.get("/", function (req, res, next) {
+    const emailAddress = basicAuth(req).name;
+    User.find({emailAddress: emailAddress}, function (err, user) {
+        if (err) {
+            next(err);
+        }
+        res.json(user);
+    })
 
 });
 
