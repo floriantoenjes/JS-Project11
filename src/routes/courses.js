d2f21768d6e455c9b9d5e574be18b9ea44ecf5cd
Florian Tönjes <skatepainter@yahoo.de>
2017-05-17 12:30:29 +0200

    Add courses route

---
+++ src/routes/courses.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+const Course = require("../models/course");
+
+router.get("/", function (req, res, next) {
+
+});
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
